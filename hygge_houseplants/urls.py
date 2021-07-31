4cc9ae50b24a26a9023cde4861eefec5e27c0367
nickassafkirk <nickassafkirk@gmail.com>
2021-07-31 18:22:36 +0000

    add social.urls project.urls file

--- hygge_houseplants/urls.py
+++ hygge_houseplants/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('index.urls')),
+    path('social/', include('social.urls')),
 ]

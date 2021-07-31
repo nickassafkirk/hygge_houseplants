53485fd87d3aa4c04047a316f41dd74b5d1e755e
nickassafkirk <nickassafkirk@gmail.com>
2021-07-31 18:31:35 +0000

    Add add_social_account view

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import SocialMediaProfile
-
+from .forms import SocialForm
 
 
 def social(request):
@@ -12,3 +12,20 @@ def social(request):
     return render(request, 'social/social.html', context)
 
 
+def add_social_account(request):
+    if request.method == "POST":
+        form = SocialForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('social'))
+        else:
+            return HttpResponseRedirect('/failed/')
+    else:
+        form = SocialForm()
+
+    template = 'social/add_social.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)

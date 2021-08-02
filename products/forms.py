bcf7f4ec400b76adafeb372fc37efcbb5babb942
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 21:56:25 +0000

    Add catgories select to add product form

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .widgets import CustomClearableFileInput
 
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -12,10 +12,18 @@ class ProductForm(forms.ModelForm):
         exclude = ('created_date',)
 
     image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
+    has_variants = forms.BooleanField(required=False)
+    available = forms.BooleanField(required=True, initial=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        display_names = [(category.id, category.get_display_name()) for category in categories]
 
+        cat_field = self.fields['category']
+
+        cat_field.choices = display_names
+        cat_field.widget.attrs.update({'class': 'form-select'})
         self.fields['name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'product-form-field'

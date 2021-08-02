5fbb429ee7bc50d6efc387b489f44d042f0254df
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 17:39:18 +0000

    Add add_product view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 
 def products(request):
@@ -9,6 +10,16 @@ def products(request):
 
     context = {
         'products': products,
+    }
+
+    return render(request, template, context)
+
+
+def add_product(request):
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
     }
 
     return render(request, template, context)

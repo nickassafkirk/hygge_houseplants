1c54ef51ec9d2e3aa231b9057ee3e7f49899218a
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 18:21:28 +0000

    replace placeholders with labels for consitent styling

--- products/forms.py
+++ products/forms.py
@@ -15,23 +15,7 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'name': 'Product Name',
-            'category': 'Product Category',
-            'sku': 'Product SKU',
-            'description': 'Description',
-            'price': 'Price(€)',
-            'quantity': 'Stock Quantity',
-            'image_url': 'Image URL',
-        }
 
         self.fields['name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'image' and field != 'has_variants' and field != 'available':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'product-form-field'
-            self.fields[field].label = False

ea4bea187e06628be24f22eb70e096c5d2714fa2
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 16:33:48 +0000

    Add product urls

--- hygge_houseplants/urls.py
+++ hygge_houseplants/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('index.urls')),
     path('social/', include('social.urls')),
+    path('products/', include('products.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

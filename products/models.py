2a4dda22380d017a2700f2150a02901b7feeb3a3
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 14:20:14 +0000

    Add products Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+        name = models.Charfield(max_length=254)
+        formatted_name = models.Charfield(max_length=254, null=True, blank=True)
+
+        def __str__(self):
+            return self.name
+
+        def  get_formatted_name(self):
+            return self.formatted_name
+
+
+

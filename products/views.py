7b6ab7c385076eb3deb5862ed268de1792f9409f
nickassafkirk <nickassafkirk@gmail.com>
2021-08-02 16:36:17 +0000

    Add view for products page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def products(request):
+    products = Product.objects.all()
+
+    template = 'products/products.html'
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, template, context)

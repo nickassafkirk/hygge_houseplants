5b33bfaafa3e53a8865d388f5d152fab346bdd16
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 23:43:49 +0000

    Elect product fields to be displayed in admnin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'sku',
+        'category',
+        'price',
+        'image',
+        'available',
+    )
+
+    ordering = ('id',)
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)

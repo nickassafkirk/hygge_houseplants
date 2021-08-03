abb6e440ebf48e8325c73e5a6bbd63d96469c4ca
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 18:35:03 +0000

    Add social contexts to settings to enable use across site

--- hygge_houseplants/settings.py
+++ hygge_houseplants/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'social.contexts.social',
             ],
         },
     },

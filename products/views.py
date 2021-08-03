5c1d71952cb747417655483426c509626ddb26e9
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 00:37:25 +0000

    Fix typo

--- products/views.py
+++ products/views.py
@@ -35,7 +35,7 @@ def products(request):
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(nam__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']

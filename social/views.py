dcabe1a59d632f04325588e96b97c9bc39009c11
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 18:34:19 +0000

    Delete social view now in contexts

--- social/views.py
+++ social/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import SocialMediaProfile
 from .forms import SocialForm
-
-
-def social(request):
-    social_accounts = SocialMediaProfile.objects.all()
-    context = {
-        'social_accounts': social_accounts,
-    }
-    return render(request, 'social/social.html', context)
 
 
 def add_social_account(request):

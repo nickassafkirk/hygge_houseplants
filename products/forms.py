baaf1bb5a71a636a50e74cf4d9d35ef22b128c48
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 19:33:07 +0000

    Add form for Variant object

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .widgets import CustomClearableFileInput
 
 
-from .models import Product, Category
+from .models import Product, Category, Variant
 
 
 class ProductForm(forms.ModelForm):
@@ -21,9 +21,20 @@ class ProductForm(forms.ModelForm):
         display_names = [(category.id, category.get_display_name()) for category in categories]
 
         cat_field = self.fields['category']
+        variants_field = self.fields['has_variants']
+
+        variants_field.widget.attrs['id'] = "has_variants"
 
         cat_field.choices = display_names
         cat_field.widget.attrs.update({'class': 'form-select'})
         self.fields['name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'product-form-field'
+
+
+class VariantForm(forms.ModelForm):
+
+    class Meta:
+        model = Variant
+        fields = '__all__'
+

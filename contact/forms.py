802cf22ca0ada8f3d20a3f1468c4d149b9399036
nickassafkirk <nickassafkirk@gmail.com>
2021-08-03 12:14:18 +0000

    Add additional contact form fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
+    from_email = forms.EmailField(required=True, label="Email")
+    name = forms.CharField(label="Name", required=False)
+    phone = forms.IntegerField(max_value=15, required=False)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)

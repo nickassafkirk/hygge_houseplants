a815945be29acbe879b8e4f8021fb1defe3a2f1b
nickassafkirk <nickassafkirk@gmail.com>
2021-08-07 12:20:26 +0000

    Add / to paths to prevent errors

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
-    path('product/<product_id>', views.single_product, name='single_product'),
+    path('product/<product_id>/', views.single_product, name='single_product'),
     path('add/', views.add_product, name='add_product'),
-    path('add_variants/<product_id>', views.add_variants, name='add_variants'),
-    path('delete/<int:product_id>', views.delete_product, name='delete_product'),
+    path('add_variants/<product_id>/', views.add_variants, name='add_variants'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]

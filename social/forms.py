859d6f18afcd7c81b9d736137afe857811ed66c1
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 02:27:25 +0000

    Render user friendly names for the logo dropdown to improve UX

--- social/forms.py
+++ social/forms.py
@@ -14,3 +14,10 @@ class SocialForm(forms.ModelForm):
     class Meta:
         model = SocialMediaProfile
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        icons = SocialMediaIcon.objects.all()
+        display_names = [(i.id, i.name) for i in icons]
+
+        self.fields['icon'].choices = display_names

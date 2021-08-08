1c6faacb53c192fe4a2acfbd3959a6debfabf139
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 02:33:53 +0000

    Sort social medial icons alphabetically

--- social/contexts.py
+++ social/contexts.py
@@ -2,7 +2,7 @@ from .models import SocialMediaProfile, SocialMediaIcon
 
 
 def social(request):
-    social_accounts = SocialMediaProfile.objects.all()
+    social_accounts = SocialMediaProfile.objects.all().order_by('name')
     social_icons = SocialMediaIcon.objects.all()
     context = {
         'social_accounts': social_accounts,

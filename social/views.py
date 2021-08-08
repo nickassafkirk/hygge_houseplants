f811c4f143849cc5aa58b1661f1c9d6d2bda12ae
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 15:07:11 +0000

    Rename edit_social_account view and handle post requests

--- social/views.py
+++ social/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.forms import modelformset_factory
+from django.contrib import messages
 from .forms import SocialForm, IconForm
 from .models import SocialMediaProfile
 
 
-def add_social_account(request):
+def edit_social_account(request):
     FormSet = modelformset_factory(
         SocialMediaProfile,
         form=SocialForm,
-        extra=1,
+        extra=0,
         can_delete=True,
         )
-    if request.method == "POST":
-        form = SocialForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            return redirect('index')
+    if request.method == "POST":
+        formset = FormSet(request.POST)
+        print('formset check valid')
+        if formset.is_valid():
+            formset = formset.save()
+            print('formset saved')
+            messages.success(request, 'Social accounts updated successfully!')   
         else:
-            print('form is invalid')
+            print(formset.non_form_errors())
+            print(formset.errors)
+            messages.error(request, 'invalid formset')
+        return redirect(reverse('edit_social_account'))
     else:
         formset = FormSet()
 
-    template = 'social/add_social.html'
-    context = {
-        'formset': formset,
-    }
-    return render(request, template, context)
+        template = 'social/edit_social.html'
+        context = {
+            'formset': formset,
+        }
+        return render(request, template, context)
 
 
 def add_icon(request):

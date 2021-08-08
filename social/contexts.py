936cc14c8e2d01c0feeb728ed31b3b8cda77f168
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 01:27:07 +0000

    add social_icon to context and make migrations

--- social/contexts.py
+++ social/contexts.py
@@ -1,9 +1,11 @@
-from .models import SocialMediaProfile
+from .models import SocialMediaProfile, SocialMediaIcon
 
 
 def social(request):
     social_accounts = SocialMediaProfile.objects.all()
+    social_icons = SocialMediaIcon.objects.all()
     context = {
         'social_accounts': social_accounts,
+        'icons': social_icons,
     }
     return context

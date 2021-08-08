607041f4b29088cff72470538c42d2c7382f9b65
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 01:20:35 +0000

    Add model for SocialMediaIcon object

--- social/models.py
+++ social/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
+class SocialMediaIcon(models.Model):
+    name = models.CharField(max_length=25)
+    icon = models.CharField(max_length=50)
+
+
 class SocialMediaProfile(models.Model):
     name = models.CharField(max_length=25)
     link = models.URLField(max_length=1024, null=True, blank=True)
-    icon = models.CharField(max_length=20, null=True, blank=True)
+    icon = models.ForeignKey(
+        SocialMediaIcon,
+        related_name='variants',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+        )

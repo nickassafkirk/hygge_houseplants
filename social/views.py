29344e33e0020b916c79e18c0a6a8237ffda837a
nickassafkirk <nickassafkirk@gmail.com>
2021-08-08 01:20:00 +0000

    Add functionality to create new social providers

--- social/views.py
+++ social/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .forms import SocialForm
+from .forms import SocialForm, IconForm
 
 
 def add_social_account(request):
@@ -15,6 +15,25 @@ def add_social_account(request):
         form = SocialForm()
 
     template = 'social/add_social.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
+def add_icon(request):
+    if request.method == "POST":
+        form = IconForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('add_icon')
+        else:
+            print('form is invalid')
+    else:
+        form = IconForm()
+
+    template = 'social/add_icon.html'
     context = {
         'form': form,
     }

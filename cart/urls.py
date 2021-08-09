727ce1f24648016f5acdbe9cce4f39111c9a7383
nickassafkirk <nickassafkirk@gmail.com>
2021-08-09 00:57:25 +0000

    Retrieve key information from add-to-cart form

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
+    path('add/<product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -6,3 +6,18 @@ from products.models import Product, Variant
 
 def view_cart(request):
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    print(request.POST)
+    product = get_object_or_404(Product, pk=product_id)
+    quantity = request.POST.get('add-to-cart-quantity')
+    variant = None
+
+    if 'variant-select' in request.POST:
+        variant_id = request.POST['variant-select']
+        variant = get_object_or_404(Variant, pk=variant_id)
+        print(product, quantity, variant)
+    else:
+        print(product, quantity)
+    return redirect('single_product', product_id=product.id)

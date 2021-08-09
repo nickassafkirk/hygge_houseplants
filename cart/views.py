a91a106ed2c93f8dc5dec825364acb82b8fc6f1d
nickassafkirk <nickassafkirk@gmail.com>
2021-08-09 21:35:27 +0000

    write code plan

--- cart/views.py
+++ cart/views.py
@@ -14,6 +14,19 @@ def add_to_cart(request, product_id):
     quantity = request.POST.get('add-to-cart-quantity')
     variant = None
 
+    # get cart session variable or create if it doesn't exist
+
+    # check if: product has variants
+    # --check if a specific variant is in the cart
+    # --if it is increase the quantity by quantity if not add it to cart
+
+    # else: check if product is in the cart already
+    # if it is increase the quantity by quantity if not add it to cart
+
+    # save the cart session cookie with updated values
+
+    # redirect to same page
+
     if 'variant-select' in request.POST:
         variant_id = request.POST['variant-select']
         variant = get_object_or_404(Variant, pk=variant_id)

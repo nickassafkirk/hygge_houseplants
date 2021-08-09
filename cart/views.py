c8f4127405b74ef5593c923c38bd92f008b2928b
nickassafkirk <nickassafkirk@gmail.com>
2021-08-09 22:15:39 +0000

    Add add_to_cart functionality

--- cart/views.py
+++ cart/views.py
@@ -14,23 +14,40 @@ def add_to_cart(request, product_id):
     quantity = request.POST.get('add-to-cart-quantity')
     variant = None
 
-    # get cart session variable or create if it doesn't exist
+    # get cart session variable or create empty dict if it doesn't exist
+    cart = request.session.get('cart', {})
 
     # check if: product has variants
-    # --check if a specific variant is in the cart
-    # --if it is increase the quantity by quantity if not add it to cart
-
-    # else: check if product is in the cart already
-    # if it is increase the quantity by quantity if not add it to cart
-
-    # save the cart session cookie with updated values
-
-    # redirect to same page
-
     if 'variant-select' in request.POST:
         variant_id = request.POST['variant-select']
         variant = get_object_or_404(Variant, pk=variant_id)
-        print(product, quantity, variant)
+
+    if variant:
+        # check if product is in the cart already
+        if product_id in list(cart.keys()):
+            # check if a specific variant is in the cart
+            if variant in cart[product_id]['product_variants'].keys():
+                # if variant exists increase it's quantity
+                cart[product_id]['product_variants'][variant] += quantity
+                print(cart[product_id]['product_variants'][variant])
+            else:
+                # if not add prodcut variant to cart
+                cart[product_id]['product_variants'][variant] = quantity
+        else:
+            # create product and assign it's variants keys and values
+            cart[product_id] = {'product_variants': {variant: quantity}}
     else:
-        print(product, quantity)
+        # Handle product without variants
+        if product_id in list(cart.keys()):
+            # if product in cart, increase it's quantity value
+            cart[product_id] += quantity
+        else:
+            # else add it to cart dict and assign it's quantity value
+            cart[product_id] = quantity
+
+    # save the cart session cookie with updated values
+    print(cart)
+    request.session['cart'] = cart
+
+    # redirect to same product page
     return redirect('single_product', product_id=product.id)

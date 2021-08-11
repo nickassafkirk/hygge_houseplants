8901d2531ea7cf132089ef344c82adf0de6d16b8
nickassafkirk <nickassafkirk@gmail.com>
2021-08-11 13:52:49 +0000

    Correct typo in url arguement to prevent errors

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add/<product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('update/<product_id>/', views.update_cart_qty, name='update_cart_qty'),
+    path('update/<item_id>/', views.update_cart_qty, name='update_cart_qty'),
     path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]

3c645e2006c503946a793a23ed61b422068d97d8
nickassafkirk <nickassafkirk@gmail.com>
2021-08-12 21:13:27 +0000

    Configure Postgress DB settings for use on deployed app

--- hygge_houseplants/settings.py
+++ hygge_houseplants/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
@@ -125,12 +126,17 @@ WSGI_APPLICATION = 'hygge_houseplants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

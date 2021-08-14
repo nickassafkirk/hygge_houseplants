7abb543f327f799765a4a83d9317f7c982aaa905
nickassafkirk <nickassafkirk@gmail.com>
2021-08-14 18:46:59 +0000

    Add fixtures folder to products

--- hygge_houseplants/settings.py
+++ hygge_houseplants/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -32,9 +33,9 @@ DEBUG = DEVELOPMENT
 
 if DEVELOPMENT:
     ALLOWED_HOSTS = ['localhost']
+
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_URL')]
-
 
 # Application definition
 

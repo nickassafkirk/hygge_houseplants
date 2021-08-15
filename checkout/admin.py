22b5f2cc280cd81b731078dd8937831a1a6df03e
nickassafkirk <nickassafkirk@gmail.com>
2021-08-15 20:23:33 +0000

    register OrderLineItems in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+
+    readonly_fields = ('lineitem_total',)
+
+
 class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = (
         'order_number',
         'date',

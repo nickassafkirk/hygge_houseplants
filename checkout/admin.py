cf823f7e29c267af2969dfebe87013135e8bfaf8
nickassafkirk <nickassafkirk@gmail.com>
2021-08-15 20:07:55 +0000

    register Order model admin and configure display fields

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'item_total',
+        'order_total',
+    )
+
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'street_Address1',
+        'street_Address2',
+        'city_or_town',
+        'state_or_province',
+        'postcode',
+        'country',
+        'delivery_cost',
+        'item_total',
+        'order_total',
+    )
+
+    ordering = ('order_number',)
+
+admin.site.register(Order, OrderAdmin)

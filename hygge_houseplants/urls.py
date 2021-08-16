130346904d0b22ad11ab55a69a66be882de65e9d
nickassafkirk <nickassafkirk@gmail.com>
2021-08-16 15:16:45 +0000

    Add intitial checkout url

--- hygge_houseplants/urls.py
+++ hygge_houseplants/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('contact/', include('contact.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

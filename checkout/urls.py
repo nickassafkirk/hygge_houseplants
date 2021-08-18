fb2527a84b7145d33ddd443e92701e0d055cce5c
nickassafkirk <nickassafkirk@gmail.com>
2021-08-18 18:04:21 +0000

    Bug fix: Add missing / to url

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,16 +5,9 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name="checkout"),
 
-    path(
-        'success/<order_number>/',
-        views.checkout_success, name="checkout_success"
-        ),
+    path('success/<order_number>/', views.checkout_success, name="checkout_success"),
 
-    path(
-        'cache_checkout_data',
-        views.cache_checkout_data,
-        name='cache_checkout_data'
-        ),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
 
     path('wh/', webhook, name='webhook'),
 ]

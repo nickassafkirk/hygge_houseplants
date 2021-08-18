2528fe1c7a8b1bf7ce8c16bb386a23e15d8262ef
nickassafkirk <nickassafkirk@gmail.com>
2021-08-18 21:11:50 +0000

    Add functionality to add order to DB if stripe payment created but order was not saved

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
-from .models import Order
+from django.shortcuts import HttpResponse, get_object_or_404
+from .models import Order, OrderLineItem
+
+from products.models import Product, Variant
+
+import time
+import json
 
 
 class StripeWH_Handler:
@@ -24,11 +29,12 @@ class StripeWH_Handler:
         intent = event.data.object
         pid = intent.id
         cart = intent.metadata.cart
+
         save_details = intent.metadata.save_details
         accept_marketing = intent.metadata.accept_marketing
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        order_total = round(intent.data.charges[0].amount / 100, 2)
+        order_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Store none for blank address field not empty string
         for field, value in shipping_details.address.items():
@@ -41,22 +47,13 @@ class StripeWH_Handler:
 
             try:
                 order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    email__iexact=shipping_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.country,
-                    postcode__iexact=shipping_details.postal_code,
-                    city_or_town__iexact=shipping_details.city,
-                    street_address1__iexact=shipping_details.line1,
-                    street_address2__iexact=shipping_details.line2,
-                    county_or_state__iexact=shipping_details.state,
-                    order_total__iexact=order_total,
+                    stripe_pid=pid,
                 )
                 order_exists = True
                 break
 
             except Order.DoesNotExist:
-                attempt +=1
+                attempt += 1
                 time.sleep(1)
         if order_exists:
             return HttpResponse(
@@ -67,18 +64,20 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    email=shipping_details.email,
+                    email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    country=shipping_details.country,
-                    postcode=shipping_details.postal_code,
-                    city_or_town=shipping_details.city,
-                    street_address1=shipping_details.line1,
-                    street_address2=shipping_details.line2,
-                    county_or_state=shipping_details.state,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    city_or_town=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county_or_state=shipping_details.address.state,
                     order_total=order_total,
+                    original_cart=cart,
+                    stripe_pid=pid
                 )
 
-                for product_id, product_data in json.loads(cart).items():   
+                for product_id, product_data in json.loads(cart).items():
                     product = get_object_or_404(Product, pk=product_id)
 
                     # handle lineitem without variants
@@ -90,18 +89,19 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
 
-                # handle lineitem with variant
-                else:
-                    # iterate through product_data to extract variant_id & Qty
-                    for variant_id, quantity in product_data["product_variants"].items():
-                        variant = get_object_or_404(Variant, pk=variant_id)
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            variant=variant,
-                            quantity=quantity,
-                        )
-                        order_line_item.save()
+                    # handle lineitem with variant
+                    else:
+                        # iterate through product_data to extract variant_id & Qty
+                        for variant_id, quantity in product_data["product_variants"].items():
+                            variant = get_object_or_404(Variant, pk=variant_id)
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                variant=variant,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+
             except Exception as e:
                 if order:
                     order.delete()

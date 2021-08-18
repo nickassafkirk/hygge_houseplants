4dc9a3f5a5321a2554ef593fee732a82bf9381e7
nickassafkirk <nickassafkirk@gmail.com>
2021-08-18 11:38:40 +0000

    Fix environment issues and update requirements.txt

--- hygge_houseplants/settings.py
+++ hygge_houseplants/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
+if os.path.exists("env.py"):
+    import env
+
 
 DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
 
@@ -26,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEVELOPMENT

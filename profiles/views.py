cbdd496c1988a23677f4179a5f290e7ce352f4f1
nickassafkirk <nickassafkirk@gmail.com>
2021-08-19 15:35:40 +0000

    Add initial profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import Order
+
+
+@login_required
+def profile(request):
+    """ Display the user's profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+    # profile is the user who's info we wish to get or update from db
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+        else:
+            messages.error(request,'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
+    }
+
+
+

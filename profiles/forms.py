b309d90a7a72b11193b0471973d8cd4f58fd5823
nickassafkirk <nickassafkirk@gmail.com>
2021-08-19 15:10:36 +0000

    add userProfile form

---
+++ profiles/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto generated
+        labels and set autofocus on first field
+        """
+
+        # We use placeholders to overide default labels for form inputs
+        super().__init__(*args, **kwargs)
+        labels = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_city_or_town': 'City/Town',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county_or_state': 'County/State',
+        }
+
+        # set autofocus on full name field
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    label = f'{labels[field]} *'  # puts a star on the placeholder if it's required
+                else:
+                    label = label[field]
+                self.fields[field].label = label
+            self.fields[field].widget.attrs['class'] = 'form-control'  # adds a css class
